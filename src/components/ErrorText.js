1b445f7748719f314cba540bb804024784bcf1e1
Prasanth Reddy CV <67952842+PrasanthReddy-Chittapu6683@users.noreply.github.com>
2021-06-22 19:28:19 +0530

    2nd commit

---
+++ src/components/ErrorText.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ErrorText = (props) => {
+    return (
+        <div className='error'>
+            {props.children}
+        </div>
+    )
+}
+
+export default ErrorText
--- src/components/FormikComponent.js
+++ src/components/FormikComponent.js
@@ -1,14 +1,22 @@
 import React from 'react'
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik, Form, Field, ErrorMessage, FieldArray, FastField } from 'formik';
 import * as Yup from 'yup'
-
+import ErrorText from "./ErrorText";
 
 
 const FormikComponent = () => {
     const initialValues = {
-        name: 'REDDY',
+        name: '',
         email: '',
-        channel: ''
+        channel: '',
+        comments: '',
+        address: '', address_two: '',
+        social: {
+            facebook: '',
+            twitter: ''
+        },
+        phoneNumbers: ['', ''],
+        otherLinks: ['']
     }
 
     const onSubmit = (values) => {
@@ -18,7 +26,10 @@ const FormikComponent = () => {
     const validationSchema = Yup.object({
         name: Yup.string().required('Name is Required - YUP'),
         email: Yup.string().email('Invalid email format').required('email is Required - YUP'),
-        channel: Yup.string().required('channel is Required - YUP')
+        channel: Yup.string().required('Channel is Required - YUP'),
+        // comments: Yup.string().required('Comments is Required - YUP'),
+        address: Yup.string().required('Address is Required - YUP'),
+        address_two: Yup.string().required('Address 2 is Required - YUP')
     })
 
     /** Custome Validation logic */
@@ -49,9 +60,14 @@ const FormikComponent = () => {
     //     // validate: validate
     // })
 
-    // console.log("form Values::: >>> ", formData.values)
-    // console.log("ERROR::::", formData.errors)
-    // console.log("Visited Fields::::", formData.touched)
+
+    const validateCommnents = (values) => {
+        let error;
+        if (!values) {
+            error = 'Comments Requrired (Field Level Error validation)'
+        }
+        return error;
+    }
     return (
         <Formik
             initialValues={initialValues}
@@ -63,20 +79,135 @@ const FormikComponent = () => {
                     <label htmlFor="name">Name</label>
                     <Field type="text"
                         id="name" name='name' />
-                    <ErrorMessage className='error' name='name' />
+                    {/* Render error message using custome component using 'component' props */}
+                    <ErrorMessage component={ErrorText} name='name' />
                 </div>
                 <div className="form-control">
                     <label htmlFor="email">Email</label>
                     <Field type="email"
                         id="email" name='email' />
-                    <ErrorMessage name='email' />
+                    {/* Render Props pattern for Error Messages */}
+                    <ErrorMessage name='email' >
+                        {
+                            errMsg =>
+                                <div className='error'> {errMsg}</div>
+                        }
+                    </ErrorMessage>
                 </div>
                 <div className="form-control">
                     <label htmlFor="channel">Channel</label>
                     <Field type="text"
                         id="channel" name='channel' />
-                    <ErrorMessage name='channel' />
+                    <ErrorMessage className='error' component='div' name='channel' />
                 </div>
+                <div className="form-control">
+                    <label htmlFor="comments">Comments</label>
+                    <Field as='textarea' validate={validateCommnents}
+                        id="comments" name='comments' >
+                    </Field>
+                    <ErrorMessage component={ErrorText} name='comments' />
+                </div>
+
+                {/*START: FastField components used for Performance issues */}
+                <div className="form-control">
+                    <label htmlFor="address">Address 1</label>
+                    <FastField name='address' >
+                        {
+                            (props) => {
+                                console.log("FAST FIELD RENDER")
+
+                                const { field, form, meta } = props;
+                                return (
+                                    <div>
+                                        <input type='text' id='address' placeholder='using Fast Field Component' {...field} />
+                                        {/* {meta.touched && meta.error ? <div>{meta.error} </div> : null} */}
+                                        <ErrorMessage name='address' >
+                                            {
+                                                errMsg =>
+                                                    <div className='error'> {errMsg}</div>
+                                            }
+                                        </ErrorMessage>
+                                    </div>
+                                )
+                            }
+                        }
+                    </FastField>
+
+                </div>
+
+
+
+                {/*END: FastField components used for Performance issues */}
+
+                {/*START: Nested object for Facebook and Twitter fields */}
+                <div className="form-control">
+                    <label htmlFor="facebook">Facebook Profile</label>
+                    <Field type='text'
+                        id="facebook" name='social.facebook' >
+                    </Field>
+                    <ErrorMessage className='error' component='div' name='social.facebook' />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="twitter">Twitter Profile</label>
+                    <Field type='text'
+                        id="twitter" name='social.twitter' >
+                    </Field>
+                    <ErrorMessage className='error' component='div' name='social.twitter' />
+                </div>
+                {/*END: Nested object for Facebook and Twitter fields */}
+
+                {/*START: ARRAY object for Phone Number fields */}
+
+                <div className="form-control">
+                    <label htmlFor="phoneNumbers">Phone Numbers1</label>
+                    <Field type='text'
+                        id="phoneNumbers" name='phoneNumbers[0]' >
+                    </Field>
+                    <ErrorMessage className='error' component='div' name='phoneNumbers[0]' />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="phoneNumbers">Phone Numbers2</label>
+                    <Field type='text'
+                        id="phoneNumbers" name='phoneNumbers[1]' >
+                    </Field>
+                    <ErrorMessage className='error' component='div' name='phoneNumbers[1]' />
+                </div>
+                {/*END: ARRAY object for Phone Number fields */}
+
+                {/*START: FieldArray components to display dynamically */}
+                <div className='form-control'>
+                    <label> Other URLS</label>
+                    {/* use Render props pattern to get better control on the field */}
+                    <FieldArray name='otherLinks'>
+                        {
+                            fieldArrayProps => {
+                                const { push, remove, form } = fieldArrayProps;
+                                const { values } = form;
+                                const { otherLinks } = values;
+                                return <div>
+                                    {
+                                        otherLinks.map((lnk, index) => (
+                                            <div key={index}>
+                                                <Field name={`otherLinks[${index}]`}></Field>
+                                                {index > 0 &&
+                                                    <button type='button' onClick={() => remove(index)}>{' '}-{' '}</button>
+                                                }
+                                                <button type='button' onClick={() => push('')}>{' '}+ {' '}</button>
+
+                                            </div>
+                                        ))
+                                    }
+                                </div>
+                            }
+                        }
+                    </FieldArray>
+                </div>
+
+
+
+
+                {/*END: FieldArray components to display dynamically */}
+
                 <button type='submit'> Submit</button>
             </Form>
         </Formik>
